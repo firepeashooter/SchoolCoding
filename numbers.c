aff54b2416089e90639931401bd8edc63d8199cb
Ben Braun <20brb7@linux6.caslab.queensu.ca>
2023-02-02 14:55:24 -0500

    THis is 221 Assignment

--- numbers.c
+++ numbers.c
@@ -4,6 +4,8 @@
 
 
 int subtract2sc_issafe(int x, int y){
+    int diff = x-y;
+
     //Overflows if x and ys sign are different and if the result has the same sign as y
     if( (x > 0 && y < 0) || (x < 0 && y > 0)){
     
@@ -16,16 +18,22 @@ int subtract2sc_issafe(int x, int y){
         }
     }
 
-    //What if both sign are the same?? How do we find overflow?
-
+    //Twos compliment subtraction never overflows when the signs are the same  therefore we always return 1
+    return 1;
 }
 
 int main (int argc, char* argv[]){
 
 //Tests Overflow
-printf(subtract2sc_issafe(2147483647, -5));
+printf("%i\n", subtract2sc_issafe(2147483647, -5));
 
 //Tests Underflow
 printf("%i\n", subtract2sc_issafe(-2147483648, 5));
+
+//Tests Not Overflow if signs are different
+printf("%i\n", subtract2sc_issafe(-10, 4));
+
+//Tests Not overflow if the signs are the same
+printf("%i\n", subtract2sc_issafe(10, 4));
 }
 
