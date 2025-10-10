54e76463489c0c0420e903769ded8eb0c429d30f
Ben Braun <benjamin.braun2003@gmail.com>
2025-10-10 15:32:04 -0400

    IDEK man

--- CISC220/CISC220_A3/Q2/chkdirs.sh
+++ CISC220/CISC220_A3/Q2/chkdirs.sh
@@ -9,15 +9,20 @@ if [[ -n $spaces ]]; then
   echo "$spaces"
 fi
 
-spaces=$(find . -maxdepth 1 -type d -regex '.* .*' -print | sort)
-if [[ -n $spaces ]]; then
+# list directories with unusual character in their name
+# anything that is not a - _ lowercase uppercase digit or space
+# Regex the above and then negate it
+unusual=$(find . -maxdepth 1 -type d -regex '..[^-_[:lower:][:upper:][:digit:][:space:]].*' -print | sort)
+echo $unusual
+if [[ -n $unusual ]]; then
   echo "Filenames with unusual characters:"
-  echo "$spaces"
+  echo "$unusual"
 fi
 
-spaces=$(find . -maxdepth 1 -type d -regex '.* .*' -print | sort)
+# list directories that might be missing the _ between first and last name of a student
+# maybe check for " " followed by capital those are maybe missing them
+spaces=$(find . -maxdepth 1 -type d ! -regex '.* .*' -print | sort)
 if [[ -n $spaces ]]; then
-  echo "Directories that should be renamed"
+  echo "Filenames with spaces:"
   echo "$spaces"
 fi
-
--- CISC220/CISC220_A3/rename.sh
+++ CISC220/CISC220_A3/rename.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+if (( $# < 3)); then
+	echo "rename.sh: too few arguments" >&2
+	exit 1
+fi
 
+#For number of arguments - 2 (the first two) rename that many files
+from=$1
+to=$2
+file=$3
+
+newName=${file/$from/$to}
+
+mv ./$file ./$newName
+
+
+#How do we check if no files were renamed?
+
+for args in $#; do
+	echo $args
+done
